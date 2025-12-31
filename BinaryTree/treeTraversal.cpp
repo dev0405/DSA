6609cfd8f2d4ec7b83d33045049a91fe7aad98a6
dev0405 <gargdk2005@gmail.com>
2025-12-31 21:04:05 +0530

    Function for Inorder and Levelorder traversal added in treeTraversal file

--- BinaryTree/treeTraversal.cpp
+++ BinaryTree/treeTraversal.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 class Node{
     public:
@@ -32,12 +33,42 @@ void preorderTraversal(Node* root){
     preorderTraversal(root->left);
     preorderTraversal(root->right);
 }
+void inorderTraversal(Node* root){
+    if(root==NULL) return;
+    inorderTraversal(root->left);
+    cout<<root->data<<" ";
+    inorderTraversal(root->right);
+}
+void levelorderTraversal(Node* root){
+    if(root==NULL) return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* curr=q.front();
+        q.pop();
+        cout<<curr->data<<" ";
+        if(curr->left!=NULL){
+            q.push(curr->left);
+        }
+        if(curr->right!=NULL){
+            q.push(curr->right);
+        }
+    }
+}
 int main(){
     vector<int> preOrder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root=buildtree(preOrder);
     cout<<"Tree created successfully"<<endl;
+    cout<<"Post Order Traversal :"<<endl;
     postorderTraversal(root);
     cout<<endl;
+    cout<<"Pre Order Traversal :"<<endl;
     preorderTraversal(root);
+    cout<<endl;
+    cout<<"In Order Traversal :"<<endl;
+    inorderTraversal(root);
+    cout<<endl;
+    cout<<"Level Order Traversal :"<<endl;
+    levelorderTraversal(root);
     return 0;
 }
